aca6ab6cbec74d6496e340f9dd41df9f5e660f58
Shivnaresh singh <141318273+shiiv-n@users.noreply.github.com>
2026-08-23 16:01:21 +0530

    Delete variale-use.tf

--- variale-use.tf
+++
@@ -1,9 +0,0 @@
-# AWS Region Configuration
-# This variable defines the AWS region where Terraform resources will be created.
-# Default region: Mumbai (ap-south-1)
-
-variable "region" {
-  description = "value fo region"
-  type = string
-  default = "ap-south-1"
-}
